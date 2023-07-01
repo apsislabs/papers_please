f8c0d6a92d349e66a9d5cdf5f5337d06db6edbb7
Wyatt Kirby <wyatt@apsis.io>
2023-07-01 13:55:25 -0400

    Add support for optional permission fallthrough

--- spec/policy_spec.rb
+++ spec/policy_spec.rb
@@ -73,6 +73,81 @@ RSpec.describe PapersPlease::Policy do
       policy = sample_policy(member)
       expect(policy.can?(:read, posts.last)).to be false
     end
+
+    context 'with fallthrough' do
+      it 'returns true when any applicable role has a passing predicate' do
+        # Only members can read this new post because it is published, but
+        # does not belong to the admin
+        new_post = Post.new
+        new_post.published = true
+
+        admin_policy = sample_fallthrough_policy(admin)
+
+        expect(admin_policy.applicable_roles.keys).to contain_exactly(:admin, :member)
+        expect(admin_policy.can?(:read, new_post)).to be true
+        expect(admin_policy.can?(:read, posts.last)).to be true
+
+        member_policy = sample_fallthrough_policy(member)
+        expect(member_policy.applicable_roles.keys).to contain_exactly(:member)
+        expect(member_policy.can?(:read, new_post)).to be true
+        expect(member_policy.can?(:read, posts.last)).to be false
+      end
+
+      it 'returns false if no roles have a passing predicate' do
+        # No one can read this post because it does not belong to the admin
+        # and it is not published
+        new_post = Post.new
+        new_post.published = false
+
+        admin_policy = sample_fallthrough_policy(admin)
+        expect(admin_policy.can?(:read, new_post)).to be false
+
+        member_policy = sample_fallthrough_policy(member)
+        expect(member_policy.can?(:read, new_post)).to be false
+      end
+    end
+
+    context 'with specific role check' do
+      it 'allows you to specify a role to check against' do
+        # Only members can read this new post because it is published, but
+        # does not belong to the admin
+        new_post = Post.new
+        new_post.published = true
+
+        admin_policy = sample_fallthrough_policy(admin)
+
+        expect(admin_policy.applicable_roles.keys).to contain_exactly(:admin, :member)
+        expect(admin_policy.can?(:read, new_post, roles: [:admin])).to be false
+        expect(admin_policy.can?(:read, new_post, roles: [:member])).to be true
+        expect(admin_policy.can?(:read, new_post)).to be true
+      end
+    end
+  end
+
+  describe '#role_that_can' do
+    it 'returns the symbolic name of the roles that can perform an action' do
+      policy_klass = Class.new(PapersPlease::Policy) do
+        def configure
+          role :member
+          role :admin
+
+          permit :admin do |role|
+            role.grant :admin, Post
+            role.grant :foo, Post
+          end
+
+          permit :member do |role|
+            role.grant :read, Post
+            role.grant :foo, Post
+          end
+        end
+      end
+
+      policy = policy_klass.new(admin)
+      expect(policy.roles_that_can(:read, Post)).to contain_exactly(:member)
+      expect(policy.roles_that_can(:admin, Post)).to contain_exactly(:admin)
+      expect(policy.roles_that_can(:foo, Post)).to contain_exactly(:admin, :member)
+    end
   end
 
   describe '#authorize!' do
@@ -87,7 +162,12 @@ RSpec.describe PapersPlease::Policy do
   def sample_policy_klass
     Class.new(PapersPlease::Policy) do
       def configure
-        role :member
+        role :admin, (proc { |u| u.admin })
+        role :member, (proc { |u| u.member })
+
+        permit :admin do |role|
+          role.grant :read, Post
+        end
 
         permit :member do |role|
           role.grant :read, Post, predicate: (proc { |u, p| u.posts.include? p })
@@ -96,7 +176,31 @@ RSpec.describe PapersPlease::Policy do
     end
   end
 
+  def sample_fallthrough_policy_klass
+    Class.new(PapersPlease::Policy) do
+
+      def configure
+        allow_fallthrough
+
+        role :member, (proc { |u| u.admin || u.member })
+        role :admin, (proc { |u| u.admin })
+
+        permit :admin do |role|
+          role.grant :read, Post, predicate: (proc { |u, p| u.posts.include?(p) })
+        end
+
+        permit :member do |role|
+          role.grant :read, Post, predicate: (proc { |u, p| p.published })
+        end
+      end
+    end
+  end
+
   def sample_policy(user)
     sample_policy_klass.new(user)
+  end
+
+  def sample_fallthrough_policy(user)
+    sample_fallthrough_policy_klass.new(user)
   end
 end
